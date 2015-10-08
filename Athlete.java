1bfc5a5bc470dffad47336642e0b43532d3c39a1
amazing-username <kdeng02@aurora.edu>
2015-10-07 20:15:03 -0500

    Updated Athlete.java
    Sorry It took so long updating this file. I woke up with a terrible fever.

--- Athlete.java
+++ Athlete.java
@@ -1,13 +1,22 @@
 package tryMe;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
 
 public class Athlete
 {
 	
 	public Athlete()
 	{
-		//something
+		
 	}
 	public Athlete(String firstName, String lastName, String gender, String athleteNumber)
 	{
@@ -22,6 +31,17 @@ public class Athlete
 	private static String gender;
 	private static String athleteNumber;
 	private static String[][] athletes;
+	private static int swimTimeMinute;
+	private static int swimTimeSecond;
+	private static int bikeTimeMinute;
+	private static int bikeTimeSecond;
+	private static int runTimeMinute;
+	private static int runTimeSecond;
+	private static int totalTimeMinute;
+	private static int totalTimeSecond;
+	private static int hours;
+	private static int minutes;
+	private static int seconds;
 	
 	public static String[][] getAthletesData()
 	{
@@ -54,6 +74,105 @@ public class Athlete
 	}
 	public static void setAthleteNumber(String athleteNumber) {
 		Athlete.athleteNumber = athleteNumber;
+	}
+	public static void setHours(double totalMinutes)
+	{
+		hours = (int)(totalMinutes / 60);
+	}
+	public static void setMinutes(double totalMinutes)
+	{
+		minutes = ((int) totalMinutes % 60);
+	}
+	public static void setSeconds(double totalMinutes)
+	{
+		double s = (totalMinutes - ((int)totalMinutes)) * 60;
+		
+		seconds = (int)s;
+	}
+	public static int getHours()
+	{
+		return hours;
+	}
+	public static int getMinutes()
+	{
+		return minutes;
+	}
+	public static int getSeconds()
+	{
+		return seconds;
+	}
+	public static void setRandomSwimTimeMinute()
+	{
+		
+		
+		swimTimeMinute = (int) (Math.random() * 23) + 7;
+	}
+	public static int getRandomSwimTimeMinute()
+	{
+		
+		return swimTimeMinute;
+	}
+	public static void setRandomSwimTimeSecond()
+	{
+		swimTimeSecond = (int) (Math.random() * 60);
+	}
+	public static int getRandomSwimTimeSecond()
+	{
+		
+		return swimTimeSecond;
+	}
+	public static void setRandomBikeTimeMinute()
+	{
+		bikeTimeMinute = (int) (Math.random() * 13) + 35;
+	}
+	public static int getRandomBikeTimeMinute()
+	{
+		
+		return bikeTimeMinute;
+	}
+	public static void setRandomBikeTimeSecond()
+	{
+		bikeTimeSecond = (int) (Math.random() * 60);
+	}
+	public static int getRandomBikeTimeSecond()
+	{
+		return bikeTimeSecond;
+	}
+	public static void setRandomRunTimeMinute()
+	{
+		runTimeMinute = (int) (Math.random() * 25) + 35;
+	}
+	public static int getRandomRunTimeMinute()
+	{
+		
+		return runTimeMinute;
+	}
+	public static void setRandomRunTimeSecond()
+	{
+		runTimeSecond = (int) (Math.random() * 60);
+	}
+	public static int getRandomRunTimeSecond()
+	{
+		
+		return runTimeSecond;
+	}
+	public static void setTotalTimeMinute(int minutes, int seconds)
+	{
+		totalTimeMinute = minutes + (int)(seconds / 60);
+	}
+	public static int getTotalTimeMinute()
+	{
+		
+		return totalTimeMinute;
+	}
+	public static void setTotalTimeSecond(int seconds)
+	{
+		totalTimeSecond = (seconds % 60);
+	}
+	public static int getTotalTimeSecond()
+	{
+		
+		return totalTimeSecond;
 	}
 	
 	public static void fillItUp(String athletes[][], int numberOfAthletes, int numberOfParameters)
@@ -118,6 +237,7 @@ public class Athlete
 	}
 	public static void print(String athletes[][])
 	{
+		Arrays.sort(athletes);
 		for (int i = 0; i < 4; i++)
 		{
 			for (int j = 0; j < 4; j++)
@@ -127,17 +247,152 @@ public class Athlete
 			System.out.println();
 		}
 	}
+	public static void assign(int amountOfAthletes)
+	{
+		Stack<Object> athletes = new Stack<>();
+		
+		new ArrayList<>();
+		new ArrayList<>();
+		Queue<Object> allAthleteQueue = new LinkedList<>();
+		
+		String gender = "";
+		String firstName;
+		String lastName;
+		int athleteNumber;
+		String[][] test = new String [3][8];
+		boolean truStory = true;
+		
+		for (int i = 0; i < amountOfAthletes; i++)
+		{
+			List<Object> athleteInfo = new ArrayList<>();
+			Scanner userInput = new Scanner(System.in);
+			
+			setRandomSwimTimeMinute();
+			setRandomSwimTimeSecond();
+			
+			setRandomBikeTimeMinute();
+			setRandomBikeTimeSecond();
+			
+			setRandomRunTimeMinute();
+			setRandomRunTimeSecond();
+			
+			setTotalTimeMinute((getRandomSwimTimeMinute() + getRandomBikeTimeMinute() + 
+					getRandomRunTimeMinute()), (getRandomSwimTimeSecond() + getRandomBikeTimeSecond() + 
+					getRandomRunTimeSecond()));
+			setTotalTimeSecond((getRandomSwimTimeSecond() + getRandomBikeTimeSecond() + getRandomRunTimeSecond()));
+			
+			test[i][0] = "" + Math.round((getTotalTimeMinute() + (getTotalTimeSecond()/60.0))*100)/100.0;
+			
+			System.out.println("Enter First Name: ");
+			firstName = userInput.nextLine();
+			test[i][1] = firstName;
+			athleteInfo.add(firstName);
+			
+			System.out.println("Enter Last Name: ");
+			lastName = userInput.nextLine();
+			test[i][2] = lastName;
+			athleteInfo.add(lastName);
+			
+			System.out.println("Enter Male for Male and Female for Female: ");
+			while (truStory)
+			{
+				gender =  userInput.nextLine();
+				if (gender.equals("Male"))
+				{
+					athleteInfo.add(gender);
+					test[i][3] = gender;
+					//truStory = false;
+					break;
+				}
+				else if (gender.equals("Female"))
+				{
+					athleteInfo.add(gender);
+					test[i][3] = gender;
+					//truStory = false;
+					break;
+				}
+				else
+					System.out.println("Does not Compute");
+					System.out.println("Try again: ");
+					
+			}
+			
+			athleteNumber = (int) (Math.random()*300);
+			athleteInfo.add(athleteNumber);
+			test[i][4] = ""+athleteNumber;
+			
+				
+			athleteInfo.add(getRandomSwimTimeMinute() + ":" + getRandomSwimTimeSecond());
+			athleteInfo.add(getRandomBikeTimeMinute() + ":" + getRandomBikeTimeSecond());
+			athleteInfo.add(getRandomRunTimeMinute() + ":" + getRandomRunTimeSecond());
+			athleteInfo.add(getTotalTimeMinute() + ":" + getTotalTimeSecond());
+			
+			athletes.add(athleteInfo);
+			allAthleteQueue.add(athleteInfo);
+			
+			
+			
+		}
+				
+		Iterator<Object> itQueue = allAthleteQueue.iterator();
+		Iterator<Object> itStack = athletes.iterator();
+		
+		
+		System.out.println("Queue Iterator: ");
+		while (itQueue.hasNext())
+		{
+			System.out.println(itQueue.next());
+		}
+		
+		System.out.println("\nStack Iterator: ");
+		while (itStack.hasNext())
+		{
+			System.out.println(itStack.next());
+		}
+		
+		
+		for (int j = 0; j < amountOfAthletes; j++)
+		{
+			Arrays.sort(test, new Comparator<String[]>() 
+			{
+				@Override
+				public int compare(String[] one, String[] two)
+				{
+					String time1 = one[0];
+					String time2 = two[0];
+					
+					return time1.compareTo(time2);
+				}
+			});
+		}
+			
+		
+		System.out.println("\nString: ");
+		for (String[] ob: test)
+		{
+			setHours(Double.parseDouble(ob[0]));
+			setMinutes(Double.parseDouble(ob[0]));
+			setSeconds(Double.parseDouble(ob[0]));
+			System.out.println( getHours()+ ":" + getMinutes() + ":" + getSeconds() + " " + ob[1] + " " + ob[2] + " " + ob[3] + " " + ob[4]);
+		}
+	}
+	
+	
+	
+	
 	public static void main(String[] args)
 	{
 	
 		final int dataEntries = 4;
 		final int numberOfAthletes = 4;
 		
-		String athletes[][] = new String [numberOfAthletes][dataEntries];
 		
-		fillItUp(athletes, numberOfAthletes, dataEntries);
 		
-		print(athletes);
+		//fillItUp(athletes, numberOfAthletes, dataEntries);
+		
+		//print(athletes);
+		
+		assign(3);
 		
 	}
 }
