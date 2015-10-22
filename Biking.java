9b0c566e6ddb2698476f74185547f2810b899f96
amazing-username <kdeng02@aurora.edu>
2015-10-21 20:11:51 -0500

    Update Biking.java

--- Biking.java
+++ Biking.java
@@ -5,8 +5,6 @@ package tryMe;
 public class Biking
 {
 	
-	
-	
 	public Biking()
 	{
 		
@@ -17,8 +15,8 @@ public class Biking
 		this.bikeTimeInSeconds = bikeTimeInSeconds;
 	}
 	
-	private double bikeTimeInMinutes;
-	private double bikeTimeInSeconds;
+	private double bikeTimeInMinutes; //This is used for converting the double into something legible
+	private double bikeTimeInSeconds; //This is used for converting the double into something legible
 	private double generatedTime;
 	
 	
@@ -39,11 +37,13 @@ public class Biking
 		return bikeTimeInSeconds;
 	}
 	
+	//Generates a random time
 	public void generateRandomTime()
 	{
 		generatedTime = 0 + (Math.random()*40);
 		generatedTime = Math.round(generatedTime * 100)/100.0;
 	}
+	//Returns the generated random time
 	public double randomTime()
 	{
 		return generatedTime;
