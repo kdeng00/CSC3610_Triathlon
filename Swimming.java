8c11c99f27c9ad716a1c697ef0b511a50ea535a9
amazing-username <kdeng02@aurora.edu>
2015-10-26 20:38:05 -0500

    Kheneahm's advice
    I took Kheneahm's advice:
    https://github.com/amazing-username/CSC3610_Triathlon/issues/2
    
    Changed the the limits of the generated time.

--- Swimming.java
+++ Swimming.java
@@ -17,6 +17,7 @@ public class Swimming
 	private double swimTimeInMinutes; //This is used to convert the doubles into somtething legible
 	private double swimTimeInSeconds; //This is used to convert the doubles into something legible
 	private double generatedTime;
+	final private int fastestSwimTimeInMinutes = 10;
 	
 	
 	public void setSwimTimeInMinutes(double minutes)
@@ -39,8 +40,9 @@ public class Swimming
 	//Generates a random time
 	public void generateRandomTime()
 	{
-		generatedTime = (1) + (Math.random()*60);
-		generatedTime = Math.round(generatedTime * 100)/100.0;
+		//Generates a random time greater than or equal to 8 but less than 68
+		generatedTime = (fastestSwimTimeInMinutes) + (Math.random()*60);
+		generatedTime = Math.round(generatedTime * 100)/100.0; //Rounds it to the nearest place
 	}
 	//Returns the generated random time
 	public double randomTime()
