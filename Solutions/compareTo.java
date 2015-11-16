1d5495428891dff015313670358c466c54b6233b
amazing-username <kdeng02@aurora.edu>
2015-11-16 02:14:54 -0600

    Update and rename compareTo to Solutions/compareTo.java

--- Solutions/compareTo.java
+++ Solutions/compareTo.java
@@ -1,7 +1,7 @@
 //This is the compareTo method that could be used to compare values in the map
 
 	 public int compareTo(AresPoint p2) {
-		 if (this.x < p2.x)
+		 if (this.x < p2.x) 
 		 return -1;
 		 else if (this.x == p2.x) { //if x coordinates are the same, compare y coord
 			 if (this.y < p2.y)
@@ -13,4 +13,4 @@
 		 }
 		 else
 			 return 1;
-		 }
+	}
