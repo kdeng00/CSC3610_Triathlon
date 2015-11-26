b5d216d62bdd87dcd49d4942676c64543ceb6ece
amazing-username <kdeng02@aurora.edu>
2015-11-26 11:09:59 -0600

    Create Running.java

---
+++ Testing/Alex/src/koternoha/Running.java
@@ -0,0 +1,52 @@
+
+public class Running extends Athlete{
+	
+	private double runningTimeInMinutes; //This is used for converting the double into something legible
+	private double runningTimeInSeconds; //This is used for converting the double into something legible
+	private double generatedTime;
+	final private int fastestRunningTimeInMinutes = 18;
+	
+	public Running()
+	{
+		
+	}
+	public Running(double runningTimeInMinutes, double runningTimeInSeconds)
+	{
+		this.runningTimeInMinutes = runningTimeInMinutes;
+		this.runningTimeInSeconds = runningTimeInSeconds;
+	}
+	
+	
+	
+	public void setRunningTimeInMinutes(double minutes)
+	{
+		runningTimeInMinutes = minutes;
+	}
+	public void setRunningTimeInSeconds(double seconds)
+	{
+		runningTimeInSeconds = seconds;
+	}
+	public double getRunningTimeInMinutes()
+	{
+		return runningTimeInMinutes;
+	}
+	public double getRunningTimeInSeconds()
+	{
+		return runningTimeInSeconds;
+	}
+	
+	//Generates the random time
+	public void generateRandomTime()
+	{
+		//generates a value greater than or equal to 18 but less than 83
+		generatedTime = fastestRunningTimeInMinutes + (Math.random() * 50);
+		generatedTime = Math.round(generatedTime*100)/100.0; //Rounds to the hundreth power
+	}
+	//Returns the generated random time
+	public double randomTime()
+	{
+		return generatedTime;
+	}
+	
+
+}
