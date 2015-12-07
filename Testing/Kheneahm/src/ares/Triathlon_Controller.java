42a97d79a2af00766734c00236b66efb1e7f6b1b
mr-robot9 <kheneahm9@yahoo.com>
2015-12-06 23:36:56 -0600

    Update Triathlon_Controller.java

--- Testing/Kheneahm/src/ares/Triathlon_Controller.java
+++ Testing/Kheneahm/src/ares/Triathlon_Controller.java
@@ -11,7 +11,7 @@ import javafx.scene.layout.BorderPane;
 
 public class Triatholon_Controller {
 	
-	@FXML
+@FXML
 	private MenuItem menuClose;
 	@FXML
 	private Label maleWinner;
@@ -22,24 +22,54 @@ public class Triatholon_Controller {
 	@FXML
 	private TableView<Triatholon_Info> tblFemale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> rankMale;
+	private TableColumn<Triatholon_Info, String> placeMale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> nameFirstMale;
+	private TableColumn<Triatholon_Info, String> numberMale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> nameLastMale;
+	private TableColumn<Triatholon_Info, String> firstNameMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> lastNameMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> runTimeMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> runSpeedMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> swimTimeMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> swimSpeedMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> bikeTimeMale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> bikeSpeedMale;
 	@FXML
 	private TableColumn<Triatholon_Info, String> totalTimeMale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> rankFemale;
+	private TableColumn<Triatholon_Info, String> placeFemale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> nameFirstFemale;
+	private TableColumn<Triatholon_Info, String> numberFemale;
 	@FXML
-	private TableColumn<Triatholon_Info, String> nameLastFemale;
+	private TableColumn<Triatholon_Info, String> firstNameFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> lastNameFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> runTimeFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> runSpeedFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> swimTimeFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> swimSpeedFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> bikeTimeFemale;
+	@FXML
+	private TableColumn<Triatholon_Info, String> bikeSpeedFemale;
 	@FXML
 	private TableColumn<Triatholon_Info, String> totalTimeFemale;
 	
 	
+	
 	private Triatholon_DBaseDriver dbaseConnection;
+	private Statement stmt;
 
 	private BorderPane rootLayout;
 	private AnchorPane controlData;
@@ -47,22 +77,36 @@ public class Triatholon_Controller {
 	public Triatholon_Controller() throws IOException{
 		
 	}
-	
-	public void initialize() throws Exception {
+public void populateDatabase() {
+		try {
 		
 		dbaseConnection = new Triatholon_DBaseDriver("jdbc:mysql://localhost:3306/ath_info", "root", "password");
 
-//		rankMale.setCellValueFactory(new PropertyValueFactory<>("Rank"));
-		nameFirstMale.setCellValueFactory(new PropertyValueFactory<>("FirstNames"));
-		nameLastMale.setCellValueFactory(new PropertyValueFactory<>("LastNames"));
+		placeMale.setCellValueFactory(new PropertyValueFactory<>("Place"));
+		numberMale.setCellValueFactory(new PropertyValueFactory<>("Number"));
+		firstNameMale.setCellValueFactory(new PropertyValueFactory<>("FirstNames"));
+		lastNameMale.setCellValueFactory(new PropertyValueFactory<>("LastNames"));
+		runTimeMale.setCellValueFactory(new PropertyValueFactory<>("RunTime"));
+		runSpeedMale.setCellValueFactory(new PropertyValueFactory<>("RunSpeed"));
+		swimTimeMale.setCellValueFactory(new PropertyValueFactory<>("SwimTime"));
+		swimSpeedMale.setCellValueFactory(new PropertyValueFactory<>("SwimSpeed"));
+		bikeTimeMale.setCellValueFactory(new PropertyValueFactory<>("BikeTime"));
+		bikeSpeedMale.setCellValueFactory(new PropertyValueFactory<>("BikeSpeed"));
 		totalTimeMale.setCellValueFactory(new PropertyValueFactory<>("TotalTime"));
 		
 		
 		tblMale.getItems().addAll(dbaseConnection.getMaleAthleteInfo());
 		
-//		rankFemale.setCellValueFactory(new PropertyValueFactory<>("Rank"));
-		nameFirstFemale.setCellValueFactory(new PropertyValueFactory<>("FirstNames"));
-		nameLastFemale.setCellValueFactory(new PropertyValueFactory<>("LastNames"));
+		placeFemale.setCellValueFactory(new PropertyValueFactory<>("Place"));
+		numberFemale.setCellValueFactory(new PropertyValueFactory<>("Number"));
+		firstNameFemale.setCellValueFactory(new PropertyValueFactory<>("FirstNames"));
+		lastNameFemale.setCellValueFactory(new PropertyValueFactory<>("LastNames"));
+		runTimeFemale.setCellValueFactory(new PropertyValueFactory<>("RunTime"));
+		runSpeedFemale.setCellValueFactory(new PropertyValueFactory<>("RunSpeed"));
+		swimTimeFemale.setCellValueFactory(new PropertyValueFactory<>("SwimTime"));
+		swimSpeedFemale.setCellValueFactory(new PropertyValueFactory<>("SwimSpeed"));
+		bikeTimeFemale.setCellValueFactory(new PropertyValueFactory<>("BikeTime"));
+		bikeSpeedFemale.setCellValueFactory(new PropertyValueFactory<>("BikeSpeed"));
 		totalTimeFemale.setCellValueFactory(new PropertyValueFactory<>("TotalTime"));
 		
 		
@@ -71,10 +115,24 @@ public class Triatholon_Controller {
 		menuClose.setOnAction((e) -> {
 			System.exit(0);
 		});
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
 		
+	}
+	
+	public void initialize() throws Exception {
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/csc3610_takehome2"
+//				, "root", "password");
+//		stmt = conn.createStatement();
+//	    stmt.executeUpdate("delete from Salary;"); //deletes data from Salary every time application is run
 		
+		populateDatabase();
 		
 		
 	}
+
 
 }
